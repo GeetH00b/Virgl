88ace2cb7b70c45bae50b00bfb0ad2556c4935c1
jesusalatorre <chuyalatorre@gmail.com>
2016-08-27 16:54:07 -0500

    bug fixes on geolocation feature

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 
 
       function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
+        var map = new google.maps.Map(document.getElementById('googleMap'), {
           center: {lat: -34.397, lng: 150.644},
           zoom: 6
         });
@@ -43,6 +43,6 @@
                               'Error: Your browser doesn\'t support geolocation.');
       }
 
-       
+       initMap();
 
 
